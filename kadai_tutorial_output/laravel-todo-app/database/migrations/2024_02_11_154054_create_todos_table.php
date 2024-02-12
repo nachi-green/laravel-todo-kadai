77da2dc612280771652d85a62a83172b3ad73e94
nachi-green <rsfml329@gmail.com>
2024-02-12 22:55:30 +0900

    kadai_tutorial_output

---
+++ kadai_tutorial_output/laravel-todo-app/database/migrations/2024_02_11_154054_create_todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('goal_id')->comstrained()->cascadeOnDelete();
+            $table->boolean('done')->defalut(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
